7ee21d6422f7d670dcafd82c7bf2b179e2561d1a
Ezequiel Maidana <ezemaidana1889@gmail.com>
2024-09-22 15:39:57 -0300

    comienzo

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>
+        HOLA
+      </h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, magnam!</p>
+    </div>
+  )
+}
+
+export default App
